d186316a926e40bd5939d6b5a23ea16cbfc78f08
Kartik Chauhan <kckartik17@gmail.com>
2018-12-20 20:26:05 +0530

    all working without authentication

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 ```npm install express-handlebars --save```<br />
 ```npm install --save mongoose```<br />
 ```npm install --save body-parser```<br />
+```npm install --save method-override```<br />
+```npm install express-session connect-flash --save```<br />
--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+const flash = require('connect-flash')
+const session = require('express-session')
+
 const app = express();
 
 
@@ -22,9 +26,28 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 //Body Parser middleware
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+//Method Override Middleware
+app.use(methodOverride('_method'));
+
+//Express session Middlewares
+app.use(session({
+  secret:'secret',
+  resave:true,
+  saveUninitialized:true
+}));
+
+app.use(flash());
+
+//Global variables
+app.use(function(req,res,next){
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+})
 
 //Index Route
 app.get('/', (req, res) => {
@@ -94,10 +117,39 @@ app.post('/ideas', (req, res) => {
     new Idea(newUser)
       .save()
       .then(Idea => {
+        req.flash('success_msg','Video Idea added')
         res.redirect('/ideas')
       })
   }
 });
+
+//Edit Form process
+app.put('/ideas/:id',(req,res) => {
+  Idea.findOne({
+    _id:req.params.id
+  })
+  .then(idea => {
+    idea.title = req.body.title;
+    idea.details = req.body.details;
+
+    idea.save()
+    .then(idea => {
+      req.flash('success_msg','Video Idea updated')
+      res.redirect('/ideas')
+    })
+  })
+})
+
+//Delete Idea
+app.delete('/ideas/:id',(req,res) => {
+  Idea.deleteOne({
+    _id:req.params.id
+  })
+  .then(() => {
+    req.flash('success_msg','Video Idea removed')
+    res.redirect('/ideas')
+  })
+})
 
 const port = 5000;
 
