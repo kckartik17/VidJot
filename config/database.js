a2a77f80e8a3e24ad74e31aab2ba0399fc549dbd
Kartik Chauhan <kckartik17@gmail.com>
2018-12-21 00:01:39 +0530

    done
    Signed-off-by: Kartik Chauhan <kckartik17@gmail.com>

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
-  module.exports = {mongoURI:'mongodb://kckartik17:web@1708@ds139934.mlab.com:39934/vidjot-prod'}
+  module.exports = {mongoURI:'mongodb://kckartik17:web1708@ds139934.mlab.com:39934/vidjot-prod'}
 }else{
   module.exports = {mongoURI:'mongodb://localhost/vidjot-dev'}
 }
