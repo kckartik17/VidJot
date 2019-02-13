d29824ad0e844001f4407c2fd8a6c5aff69583a7
Kartik Chauhan <kckartik17@gmail.com>
2019-02-13 23:49:11 +0530

    Bug fixes
    Signed-off-by: Kartik Chauhan <kckartik17@gmail.com>

--- app.js
+++ app.js
@@ -1,56 +1,55 @@
-const express = require('express');
-const path = require('path');
-const exphbs = require('express-handlebars')
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser')
-const methodOverride = require('method-override')
-const flash = require('connect-flash')
-const session = require('express-session')
-const passport = require('passport')
+const express = require("express");
+const path = require("path");
+const exphbs = require("express-handlebars");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
+const flash = require("connect-flash");
+const session = require("express-session");
+const passport = require("passport");
 
 const app = express();
 
 //Load Routes
-const ideas = require('./routes/ideas');
-const users = require('./routes/users');
-
+const ideas = require("./routes/ideas");
+const users = require("./routes/users");
 
 //Passport Config
-require('./config/passport')(passport);
+require("./config/passport")(passport);
 
 //DB config
-const db = require('./config/database')
-
+const db = require("./config/database");
 
 //Connect to mongoose
-mongoose.connect(db.mongoURI, {
-  useNewUrlParser: true
-})
-  .then(() => console.log('MongoDB connected...'))
-  .catch(err => console.log('Error in connecting MongoDB'))
-
-
+mongoose
+  .connect(db.mongoURI, {
+    useNewUrlParser: true
+  })
+  .then(() => console.log("MongoDB connected..."))
+  .catch(err => console.log("Error in connecting MongoDB"));
 
 //Handlebars Middlewares
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 //Body Parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //Static Folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 //Method Override Middleware
-app.use(methodOverride('_method'));
+app.use(methodOverride("_method"));
 
 //Express session Middlewares
-app.use(session({
-  secret: 'secret',
-  resave: true,
-  saveUninitialized: true
-}));
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true
+  })
+);
 
 //Passport middleware
 app.use(passport.initialize());
@@ -59,35 +58,33 @@ app.use(passport.session());
 app.use(flash());
 
 //Global variables
-app.use(function (req, res, next) {
-  res.locals.success_msg = req.flash('success_msg');
-  res.locals.error_msg = req.flash('error_msg');
-  res.locals.error = req.flash('error');
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
   res.locals.user = req.user || null;
   next();
-})
+});
 
 //Index Route
-app.get('/', (req, res) => {
-  const name = "Hello Kartik Chauhan";
-  res.render('index', {
+app.get("/", (req, res) => {
+  const name = "Hello Kartik";
+  res.render("index", {
     Name: name
   });
 });
 
 //About Page
-app.get('/about', (req, res) => {
-  res.render('about');
+app.get("/about", (req, res) => {
+  res.render("about");
 });
 
-
-
 //Use Routes
-app.use('/ideas', ideas);
-app.use('/users', users);
+app.use("/ideas", ideas);
+app.use("/users", users);
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
-})
+});
--- routes/ideas.js
+++ routes/ideas.js
@@ -1,106 +1,97 @@
-const express = require('express')
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 const router = express.Router();
-const { ensureAuthenticated } = require('../helpers/auth')
+const { ensureAuthenticated } = require("../helpers/auth");
 
 //Load Idea Model
-require('../models/Idea');
-const Idea = mongoose.model('ideas');
+require("../models/Idea");
+const Idea = mongoose.model("ideas");
 
 //Idea Index Page
-router.get('/', ensureAuthenticated, (req, res) => {
+router.get("/", ensureAuthenticated, (req, res) => {
   Idea.find({ user: req.user.id })
-    .sort({ date: 'desc' })
+    .sort({ date: "desc" })
     .then(ideas => {
-      res.render('ideas/index', {
+      res.render("ideas/index", {
         ideas: ideas
-      })
-    })
-
-})
+      });
+    });
+});
 
 //Add Idea Form
-router.get('/add', ensureAuthenticated, (req, res) => {
-  res.render('ideas/add');
+router.get("/add", ensureAuthenticated, (req, res) => {
+  res.render("ideas/add");
 });
 
 //Edit Idea Form
-router.get('/edit/:id', ensureAuthenticated, (req, res) => {
+router.get("/edit/:id", ensureAuthenticated, (req, res) => {
   Idea.findOne({
     _id: req.params.id
-  })
-    .then(idea => {
-      if (idea.user != req.user.id) {
-        req.flash('error_msg', 'Not Authorised');
-        res.redirect('/ideas')
-      } else {
-        res.render('ideas/edit', {
-          idea: idea
-        });
-      }
-    })
+  }).then(idea => {
+    if (idea.user != req.user.id) {
+      req.flash("error_msg", "Not Authorised");
+      res.redirect("/ideas");
+    } else {
+      res.render("ideas/edit", {
+        idea: idea
+      });
+    }
+  });
 });
 
-
 //Process Form
-router.post('/', ensureAuthenticated, (req, res) => {
+router.post("/", ensureAuthenticated, (req, res) => {
   let errors = [];
 
   if (!req.body.title) {
-    errors.push({ text: 'Please add a title' })
+    errors.push({ text: "Please add a title" });
   }
   if (!req.body.details) {
-    errors.push({ text: 'Please add some details' })
+    errors.push({ text: "Please add some details" });
   }
 
   if (errors.length > 0) {
-    res.render('/add', {
+    res.render("/add", {
       errors: errors,
       title: req.body.title,
       details: req.body.details
-    })
+    });
   } else {
     const newUser = {
       title: req.body.title,
       details: req.body.details,
       user: req.user.id
-    }
-    new Idea(newUser)
-      .save()
-      .then(Idea => {
-        req.flash('success_msg', 'Video Idea added')
-        res.redirect('/ideas')
-      })
+    };
+    new Idea(newUser).save().then(Idea => {
+      req.flash("success_msg", "Video Idea added");
+      res.redirect("/ideas");
+    });
   }
 });
 
 //Edit Form process
-router.put('/:id', ensureAuthenticated, (req, res) => {
+router.put("/:id", ensureAuthenticated, (req, res) => {
   Idea.findOne({
     _id: req.params.id
-  })
-    .then(idea => {
-      idea.title = req.body.title;
-      idea.details = req.body.details;
+  }).then(idea => {
+    idea.title = req.body.title;
+    idea.details = req.body.details;
 
-      idea.save()
-        .then(idea => {
-          req.flash('success_msg', 'Video Idea updated')
-          res.redirect('/ideas')
-        })
-    })
-})
+    idea.save().then(idea => {
+      req.flash("success_msg", "Video Idea updated");
+      res.redirect("/ideas");
+    });
+  });
+});
 
 //Delete Idea
-router.delete('/:id', ensureAuthenticated, (req, res) => {
+router.delete("/:id", ensureAuthenticated, (req, res) => {
   Idea.deleteOne({
     _id: req.params.id
-  })
-    .then(() => {
-      req.flash('success_msg', 'Video Idea removed')
-      res.redirect('/ideas')
-    })
-})
-
+  }).then(() => {
+    req.flash("success_msg", "Video Idea removed");
+    res.redirect("/ideas");
+  });
+});
 
 module.exports = router;
